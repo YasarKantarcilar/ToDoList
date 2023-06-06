901a1ed4150bd86187cf4e078cd4c2459963e71f
YasarKantarcilar <kantarcilaryasar@gmail.com>
2023-06-06 12:44:26 +0300

    S

--- firebase.tsx
+++ firebase.tsx
@@ -9,15 +9,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBW4OywLuJIBR0zWRHb2Vq_iSd3vue4dLA",
-  authDomain: "todolist-9345f.firebaseapp.com",
-  projectId: "todolist-9345f",
-  storageBucket: "todolist-9345f.appspot.com",
-  messagingSenderId: "110105486293",
-  appId: "1:110105486293:web:20637283288440db5dd35c",
-  measurementId: "G-56T7VDV9R4",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
